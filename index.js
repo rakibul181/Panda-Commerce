f51b62f55b0fc5bdbe3ddf7f047bac4ade6dd135
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-18 03:50:51 +0600

    can't  add task 6

--- index.js
+++ index.js
@@ -11,4 +11,19 @@ for(const cd of acards){
     cd.classList.remove('rounded')
     cd.style.borderRadius = "30px"
 }
- 
+
+function submite(){
+    console.log(' It is submited by successfully')
+}
+
+// const btnRemove = document.getElementById('backpack-element').addEventListener('click',function(event){
+//     event.target.parentNode.removeChild(event.target)
+// })
+const btnRemove = document.querySelectorAll('.btn-remove')
+// console.log(btnRemove)
+for(const btnR of btnRemove){
+    btnR.addEventListener('click', function(event){
+        console.log(event)
+        
+    })
+}
